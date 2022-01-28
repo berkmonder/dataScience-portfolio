63db3c8255fdfd873cf03e879c28374436bcec88
berkmonder <berkmonder@yahoo.com>
2022-01-28 18:56:57 +0300

    model & flask

--- project3/app.py
+++ project3/app.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template, request
+import model
 
 app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 @app.route('/output', methods=["POST", "GET"])
 def output():
-    number = request.args.get('numb')
+    hum = request.args.get('hum')
+    sun = request.args.get('sun')
+    press = request.args.get('press')
+    rain = request.args.get('rain')
 
-    return render_template('output.html', result=number)
+    prediction = model.classify(hum, sun, press, rain)
+
+    no_yes = {0.0: 'No.', 1.0: 'Yes.'}
+
+    result = no_yes[prediction]
+
+    return render_template('output.html', result=result)
 
 if __name__ == '__main__':
     app.run()
